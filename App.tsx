84b75b1e7126d11bca4e15ec8d2421d4c71e121e
Jordan <jordanomokeowa@hotmail.co.uk>
2024-02-23 00:14:09 +0000

    local storage test

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+
 
 export default function App() {
+
+  const [value, setValue] = useState<string | null>(null)
+
+  const getItem = async()  => {
+    await AsyncStorage.setItem("test", "testvalue");
+    var result = await AsyncStorage.getItem("test");
+    setValue(result);
+  }
+
+
+  useEffect(() => {
+    getItem()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.summaryContainer}>
+        <Text style={styles.title}>10 Days Till Payday: </Text>
+        <Text style={styles.title}>£400 remaining target</Text>
+      </View>
+      <View style={styles.bodyContainer}>
+        <Text>Spending Summary {value}</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'space-around',
+    backgroundColor:'#1F2544',
   },
+  summaryContainer: {
+    backgroundColor: '#474F7A',
+    width: '100%',
+    height: '20%',
+    justifyContent:'center',
+    alignItems: 'center'
+  },
+  bodyContainer: {
+    justifyContent:'center',
+    height: '80%',
+  },
+  title: {
+    fontWeight: 'bold',
+    color: 'white',
+  }
 });
+
+// COLOR SCHEME v0.1
+//#1F2544 -pr
+//#474F7A -sc
+//#81689D - ter
+//#FFD0EC - 4th
